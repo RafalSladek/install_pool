53b613dc80ccf21e1874141dd8b7fddbcd726e01
Rafal <rafal.sladek@gmail.com>
2017-10-30 23:44:56 +0100

    added script for install nvm

---
+++ install_nvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -uex
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | NVM_DIR=/usr/local/nvm PROFILE=/usr/local/nvm.profile bash
+
+echo 'export NVM_DIR="$HOME/.nvm"' >> /etc/profile.d/nvm.sh
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio' >> /etc/profile.d/nvm.sh
+command -v nvm
