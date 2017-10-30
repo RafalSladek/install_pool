373fc424193833964f5ee5e42fed551b39304033
Rafal <rafal.sladek@gmail.com>
2017-10-30 23:56:51 +0100

    added source cmd

--- install_nvm.sh
+++ install_nvm.sh
@@ -5,4 +5,6 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | 
 echo 'export NVM_DIR="$HOME/.nvm"' >> /etc/profile.d/nvm.sh
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /etc/profile.d/nvm.sh
 echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio' >> /etc/profile.d/nvm.sh
+
+source ~/.bashrc
 command -v nvm
