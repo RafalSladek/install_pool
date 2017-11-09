a590b9a999e03a9a609e1d413f4bce40de448449
Rafal <rafal.sladek@gmail.com>
2017-11-09 01:51:29 +0100

    added script to add pool user

---
+++ add_user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -uex
+
+user=pool
+echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+adduser --group $user
+adduser \
+   --shell /bin/bash \
+   --gecos 'User for managing pool' \
+   --ingroup $user \
+   --disabled-password \
+   --home /home/$user \
+   $user
+
+usermod -aG sudo $user
+groups $user
+
+pushd /home/$user/
+#increasing history
+sed -i "s/HISTSIZE=1000/HISTSIZE=50000/g" .bashrc
+sed -i "s/HISTFILESIZE=2000/HISTFILESIZE=50000/g" .bashrc
